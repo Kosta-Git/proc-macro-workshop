95d5b89a9bc3bf9ab8025579542f17d301d22546
kosta <k.sovaridis@inotek.be>
2025-12-08 11:54:58 +0100

    builder impl

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, Ident};
+use syn::{
+    parse_macro_input, Data, DataStruct, DeriveInput, Error, Field, Fields, Ident, LitStr, Type,
+};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -10,13 +12,16 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let expanded = match input.data {
         Data::Struct(data_struct) => {
-            let builder_function = generate_builder_function(&name, &data_struct);
-            let builder = generate_builder_struct(&name, &data_struct);
-            let builder_impl = generate_builder_impl(&name, &data_struct);
+            let fields = match get_field_structs(&data_struct) {
+                Ok(fields) => fields,
+                Err(error) => return error.into_compile_error().into(),
+            };
+
+            let builder_function = generate_builder_function(&name, &fields);
+            let builder = generate_builder_struct(&name, &fields);
+            let builder_impl = generate_builder_impl(&name, &fields);
 
             quote! {
-                use std::error::Error;
-
                 #builder_function
 
                 #builder
@@ -30,13 +35,16 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro::TokenStream::from(expanded)
 }
 
-fn generate_builder_function(struct_name: &Ident, data_struct: &DataStruct) -> TokenStream {
+fn generate_builder_function(struct_name: &Ident, fields: &[BuilderField]) -> TokenStream {
     let builder_name = builder_name(struct_name);
-    let fields = get_field_structs(data_struct)
+    let fields = fields
         .iter()
-        .map(|(name, _)| {
-            quote! {
-                #name: None
+        .map(|field| {
+            let name = field.name.clone();
+            if field.is_vec {
+                quote! { #name: std::vec::Vec::new() }
+            } else {
+                quote! { #name: std::option::Option::None }
             }
         })
         .collect::<Vec<_>>();
@@ -52,14 +60,18 @@ fn generate_builder_function(struct_name: &Ident, data_struct: &DataStruct) -> T
     }
 }
 
-fn generate_builder_struct(struct_name: &Ident, data_struct: &DataStruct) -> TokenStream {
+fn generate_builder_struct(struct_name: &Ident, fields: &[BuilderField]) -> TokenStream {
     let builder_name = builder_name(struct_name);
-
-    let fields = get_field_structs(data_struct)
+    let fields = fields
         .iter()
-        .map(|(name, ty)| {
-            quote! {
-                #name: Option<#ty>
+        .map(|field| {
+            let name = field.name.clone();
+            let ty = field.ty.clone();
+
+            if field.optional || field.is_vec {
+                quote! { #name: #ty }
+            } else {
+                quote! { #name: std::option::Option<#ty> }
             }
         })
         .collect::<Vec<_>>();
@@ -71,27 +83,28 @@ fn generate_builder_struct(struct_name: &Ident, data_struct: &DataStruct) -> Tok
     }
 }
 
-fn generate_builder_impl(struct_name: &Ident, data_struct: &DataStruct) -> TokenStream {
+fn generate_builder_impl(struct_name: &Ident, fields: &[BuilderField]) -> TokenStream {
     let builder_name = builder_name(struct_name);
 
-    let setters = get_field_structs(data_struct)
-        .iter()
-        .map(|(name, ty)| {
-            quote! {
-                pub fn #name(&mut self, #name: #ty) -> &mut Self {
-                    self.#name = Some(#name);
-                    self
-                }
-            }
-        })
-        .collect::<Vec<_>>();
+    let setters = fields.iter().map(builder_setter).collect::<Vec<_>>();
 
-    let struct_fields = get_field_structs(data_struct)
+    let struct_fields = fields
         .iter()
-        .map(|(name, _)| {
-            let err_msg = format!("Field {} is not set", name);
-            quote! {
-                #name: self.#name.ok_or(#err_msg.into())?
+        .map(|field| {
+            let name = &field.name;
+            if field.is_vec {
+                quote! {
+                    #name: self.#name.clone()
+                }
+            } else if field.optional {
+                quote! {
+                    #name: self.#name.take()
+                }
+            } else {
+                let err_msg = format!("Field {} is not set", name);
+                quote! {
+                    #name: self.#name.take().ok_or_else(|| std::boxed::Box::<dyn std::error::Error>::from(#err_msg))?
+                }
             }
         })
         .collect::<Vec<_>>();
@@ -100,12 +113,72 @@ fn generate_builder_impl(struct_name: &Ident, data_struct: &DataStruct) -> Token
         impl #builder_name {
             #(#setters)*
 
-            pub fn build(&mut self) -> Result<Command, Box<dyn Error>> {
-                #struct_name {
+            pub fn build(&mut self) -> std::result::Result<#struct_name, std::boxed::Box<dyn std::error::Error>> {
+                Ok(#struct_name {
                     #(#struct_fields),*
+                })
+            }
+        }
+    }
+}
+
+fn builder_setter(field: &BuilderField) -> TokenStream {
+    let name = &field.name;
+    let ty = &field.ty;
+
+    let should_generate_default = if let Some(each_name) = &field.each {
+        each_name != &field.name.to_string()
+    } else {
+        true
+    };
+
+    let default_impl = if should_generate_default {
+        if let Some(inner_ty) = &field.inner_type {
+            quote! {
+                pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
+                    self.#name = std::option::Option::Some(#name);
+                    self
+                }
+            }
+        } else if field.is_vec {
+            quote! {
+                pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = #name;
+                    self
+                }
+            }
+        } else {
+            quote! {
+                pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = std::option::Option::Some(#name);
+                    self
                 }
             }
         }
+    } else {
+        quote! {}
+    };
+
+    let each_impl = if let Some(each_name) = &field.each {
+        let each_ident = format_ident!("{}", each_name);
+
+        let inner_ty =
+            extract_vec_inner_type(ty).expect("each attribute should only be used on Vec<T>");
+
+        quote! {
+            pub fn #each_ident(&mut self, #each_ident: #inner_ty) -> &mut Self {
+                self.#name.push(#each_ident);
+                self
+            }
+        }
+    } else {
+        quote! {}
+    };
+
+    quote! {
+        #default_impl
+
+        #each_impl
     }
 }
 
@@ -113,13 +186,95 @@ fn builder_name(struct_name: &Ident) -> Ident {
     format_ident!("{}Builder", struct_name)
 }
 
-fn get_field_structs(data_struct: &DataStruct) -> Vec<(&Ident, &syn::Type)> {
+fn get_field_structs(data_struct: &DataStruct) -> Result<Vec<BuilderField>, Error> {
     match &data_struct.fields {
-        Fields::Named(fields_named) => fields_named
-            .named
-            .iter()
-            .map(|f| (f.ident.as_ref().unwrap(), &f.ty))
-            .collect(),
-        Fields::Unnamed(_) | Fields::Unit => vec![],
+        Fields::Named(fields_named) => {
+            let mut builder_fields = Vec::new();
+            for field in &fields_named.named {
+                let builder_field = BuilderField::new(field)?;
+                builder_fields.push(builder_field);
+            }
+            Ok(builder_fields)
+        }
+        Fields::Unnamed(_) | Fields::Unit => Ok(vec![]),
     }
+}
+
+struct BuilderField {
+    name: Ident,
+    ty: Type,
+    inner_type: Option<Type>,
+    each: Option<String>,
+    is_vec: bool,
+    optional: bool,
+}
+
+impl BuilderField {
+    fn new(field: &Field) -> Result<Self, Error> {
+        let name = field.ident.clone().unwrap();
+        let ty = field.ty.clone();
+        let inner_type = extract_option_inner_type(&ty).cloned();
+        let optional = extract_option_inner_type(&ty).is_some();
+
+        let mut each: Option<String> = None;
+        for attr in &field.attrs {
+            if attr.path().is_ident("builder") {
+                attr.parse_nested_meta(|meta| {
+                    if meta.path.is_ident("each") {
+                        let value = meta.value()?;
+                        let lit_str: LitStr = value.parse()?;
+                        each = Some(lit_str.value());
+                        Ok(())
+                    } else {
+                        Err(meta.error("expected `builder(each = \"...\")`"))
+                    }
+                })?;
+            }
+        }
+
+        let is_vec = extract_vec_inner_type(&ty).is_some();
+
+        Ok(BuilderField {
+            name,
+            ty,
+            inner_type,
+            each,
+            is_vec,
+            optional,
+        })
+    }
+}
+
+fn extract_option_inner_type(ty: &Type) -> Option<&Type> {
+    if let Type::Path(type_path) = ty {
+        if type_path.qself.is_none() {
+            if let Some(segment) = type_path.path.segments.last() {
+                if segment.ident == "Option" {
+                    if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
+                        if let Some(syn::GenericArgument::Type(inner_ty)) = args.args.first() {
+                            return Some(inner_ty);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    None
+}
+
+fn extract_vec_inner_type(ty: &Type) -> Option<&Type> {
+    if let Type::Path(type_path) = ty {
+        if type_path.qself.is_none() {
+            if let Some(segment) = type_path.path.segments.last() {
+                if segment.ident == "Vec" {
+                    if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
+                        if let Some(syn::GenericArgument::Type(inner_ty)) = args.args.first() {
+                            return Some(inner_ty);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    None
 }
